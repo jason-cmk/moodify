e9c1f6a3c678aed4eb85e5cee119306778e14779
jason-cmk <jas.kang@gmail.com>
2016-09-13 16:22:21 +1200

    Add project files and folders

---
+++ js/musichandler.js
@@ -0,0 +1,7 @@
+var myClientId = "5d2ad565a4b5742e7ac005fd2629d6e6";
+function initSC() {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
---
+++ js/musichandler.ts
@@ -0,0 +1,8 @@
+var myClientId = "5d2ad565a4b5742e7ac005fd2629d6e6";
+
+function initSC() : void {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
